33ae6be945f5b4786df7454f7332ec7634a17bef
Pratik <pr.tikgohil@gmail.com>
2020-02-19 11:12:36 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz number
-  "Fizz"
+  if number % 3 == 0 && number % 5 == 0
+    "Fizz"
+  end
 end
